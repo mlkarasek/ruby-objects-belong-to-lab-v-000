bc4e166ae99a19abac1204aa89b1f8b02c116072
Monica  Karasek <mlkarasek@gmail.com>
2017-10-27 02:15:16 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,10 @@
 class Artist
 
-  attr_accessor :name, :genre
+  attr_accessor :name
 
-  def initialize(name, genre)
+  def initialize(name)
     @name = name
-    @genre = genre
-  end
   
+  end
+
 end
